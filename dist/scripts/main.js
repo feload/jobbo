166d94b3df2e11bad7eb124414a13172a68383e4
Felipe <feload@gmail.com>
2016-03-14 20:51:41 -0600

    Create basic structure

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+var Jobbo=window.Jobbo||{};Jobbo.App=function(){return{setDom:function(){},init:function(){console.log("Jobbo init()")},run:function(){console.log("Jobbo run()"),this.init(),this.setDom()}}}();
---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+var Jobbo = window.Jobbo || {};
+Jobbo.App = (function(){
+
+    var options = {};
+
+    return {
+
+        setDom: function(){
+        },
+
+        init: function(){
+        },
+
+        run: function(options){
+            this.init();
+            this.setDom();
+        }
+
+    }
+
+})();
