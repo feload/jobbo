168fee172b0e015587ac1384667aff15c6f232b4
Felipe <feload@gmail.com>
2016-03-28 22:52:57 -0600

    Uglifying...

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
+/**
+ * Gulp file for concatenating and uglifying.
+ */
+
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    watch = require('gulp-watch');
+    uglify = require('gulp-uglify');
 
 gulp.task('main', function(){
+
+    // I know there are better ways of doing this...
+    // I'll clean this ugly list as soon as possible.
     return gulp.src(['src/scripts/lib/zepto.min.js',
         'src/scripts/modules/notifier.js',
         'src/scripts/modules/finder.js',
@@ -13,8 +19,9 @@ gulp.task('main', function(){
         'src/scripts/main.js'])
         .pipe(concat('concat.js'))
         .pipe(rename('main.js'))
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(gulp.dest('dist/scripts/'));
+
 });
 
 gulp.task('default', ['main'], function(){});
